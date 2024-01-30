9cb74518041a1524550d031afdf7883ed8ec1bb6
Kartex <kirillzhitko@gmail.com>
2024-01-30 11:54:03 +0300

    FEAT: тег рейтинга

---
+++ app/src/main/java/kanti/testonlineshop/ui/components/RatingTag.kt
@@ -0,0 +1,69 @@
+package kanti.testonlineshop.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kanti.testonlineshop.R
+import kanti.testonlineshop.ui.theme.element
+import kanti.testonlineshop.ui.theme.textBlack
+
+@Composable
+fun RatingTag(
+    modifier: Modifier = Modifier,
+    rating: Float = 0.0f,
+    reviews: Int = 0
+) = Row(
+    modifier = modifier,
+    verticalAlignment = Alignment.CenterVertically
+) {
+    val ratingColor by remember {
+        mutableStateOf(Color(0xFFF9A249))
+    }
+    Image(
+        painter = painterResource(id = R.drawable.star),
+        contentDescription = null,
+        colorFilter = ColorFilter.tint(ratingColor),
+        contentScale = ContentScale.None,
+        modifier = Modifier.size(16.dp)
+    )
+    Spacer(modifier = Modifier.width(2.dp))
+    Text(
+        text = rating.toString(),
+        style = MaterialTheme.typography.element,
+        modifier = Modifier.padding(bottom = 2.dp),
+        color = ratingColor
+    )
+    Spacer(modifier = Modifier.width(2.dp))
+    Text(
+        text = "($reviews)",
+        style = MaterialTheme.typography.element,
+        modifier = Modifier.padding(bottom = 2.dp),
+        color = MaterialTheme.colors.textBlack
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewRatingTag() {
+    RatingTag(
+        rating = 4.3f,
+        reviews = 4
+    )
+}
