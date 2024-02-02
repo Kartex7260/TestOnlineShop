79de773090457ab7770e018c52dee1842f3acac0
Kartex <kirillzhitko@gmail.com>
2024-02-02 18:18:24 +0300

    FIX: отображение кнопки скрытия в зависимости от количества строк

--- app/src/main/java/kanti/testonlineshop/ui/components/product/CollapsedIngredients.kt
+++ app/src/main/java/kanti/testonlineshop/ui/components/product/CollapsedIngredients.kt
@@ -2,6 +2,7 @@ package kanti.testonlineshop.ui.components.product
 
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -12,6 +13,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kanti.testonlineshop.ui.components.buttons.TextButton
 
 @Composable
@@ -28,6 +30,7 @@ fun CollapsedIngredients(
 ) {
     var expanded by rememberSaveable { mutableStateOf(initExpanded) }
     var textLines by rememberSaveable { mutableIntStateOf(0) }
+    var visualOverflow by rememberSaveable { mutableStateOf(false) }
     Text(
         modifier = Modifier
             .animateContentSize(),
@@ -36,9 +39,12 @@ fun CollapsedIngredients(
         overflow = TextOverflow.Ellipsis,
         onTextLayout = { textLayoutResult ->
             textLines = textLayoutResult.lineCount
+            if (!visualOverflow) {
+                visualOverflow = textLayoutResult.hasVisualOverflow
+            }
         }
     )
-    if (textLines >= 2) {
+    if (textLines >= 2 && visualOverflow) {
         TextButton(
             text = if (expanded) textExpanded else textCollapsed,
             onClick = { expanded = !expanded }
@@ -50,9 +56,8 @@ fun CollapsedIngredients(
 @Composable
 fun PreviewCollapsedIngredients() {
     CollapsedIngredients(
-        ingredients = "fjfij bejfij  joj  j fjo wef efweew gwg wr gr  grgsklgsrkgj ksfk sklrf k kw" +
-                "knf  jkjefkef kj jsfosjefjs kjlsd kfjksmk ksdk ksnfksnk kngklnl eknlkng f;e;f ;kef " +
-                "fnsjnf lsnf eklfkj slkjflksfkj slkfj lskj flksejf lsjf slegl s gl srrglg sr",
+        modifier = Modifier.width(100.dp),
+        ingredients = "test test test test test test test",
         textExpanded = "Hide",
         textCollapsed = "Show"
     )
