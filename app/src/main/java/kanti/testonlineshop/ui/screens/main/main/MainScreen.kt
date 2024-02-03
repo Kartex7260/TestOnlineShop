569527879c858a8cb081184b05a2fbd331ffd53b
Kartex <kirillzhitko@gmail.com>
2024-02-03 15:47:47 +0300

    FEAT: TopAppBar на MainScreen

--- app/src/main/java/kanti/testonlineshop/ui/screens/main/main/MainScreen.kt
+++ app/src/main/java/kanti/testonlineshop/ui/screens/main/main/MainScreen.kt
@@ -1,18 +1,45 @@
 package kanti.testonlineshop.ui.screens.main.main
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import kanti.testonlineshop.R
+import kanti.testonlineshop.ui.theme.textBlack
+import kanti.testonlineshop.ui.theme.title1
 
 @Composable
 fun MainScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Main")
+    Column {
+        Row(
+            modifier = Modifier
+            .height(46.dp)
+            .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(id = R.string.main_main),
+                style = MaterialTheme.typography.title1,
+                color = MaterialTheme.colors.textBlack
+            )
+        }
+
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Main")
+        }
     }
 }
