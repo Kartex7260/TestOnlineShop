1572266c942cb2b6544b6a94bc2fdd00f28cee40
Kartex <kirillzhitko@gmail.com>
2024-02-04 11:57:26 +0300

    OPTIMIZE: обновление данных из сети

--- app/src/main/java/kanti/testonlineshop/data/model/product/ProductRepositoryImpl.kt
+++ app/src/main/java/kanti/testonlineshop/data/model/product/ProductRepositoryImpl.kt
@@ -2,7 +2,10 @@ package kanti.testonlineshop.data.model.product
 
 import kanti.testonlineshop.data.model.product.datasource.local.ProductLocalDataSource
 import kanti.testonlineshop.data.model.product.datasource.remote.ProductRemoteDataSource
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class ProductRepositoryImpl @Inject constructor(
@@ -11,21 +14,25 @@ class ProductRepositoryImpl @Inject constructor(
 ) : ProductRepository {
 
     override val products: Flow<List<Product>> = localDataSource.products
+        .flowOn(Dispatchers.IO)
 
     override val favouriteProducts: Flow<List<Product>> = localDataSource.favouriteProducts
+        .flowOn(Dispatchers.IO)
 
     override suspend fun getProduct(productId: String): Product? {
         return localDataSource.getProduct(productId)
     }
 
     override suspend fun loadFromRemote() {
-        val products = remoteDataSource.getProducts()
-        val favouriteData = localDataSource.getFavouriteData()
-        localDataSource.deleteAll()
-        localDataSource.insert(products)
+        withContext(Dispatchers.IO) {
+            val products = remoteDataSource.getProducts()
+            val favouriteData = localDataSource.getFavouriteData()
+            localDataSource.deleteAll()
+            localDataSource.insert(products)
 
-        if (favouriteData.isNotEmpty())
-            localDataSource.setFavouriteData(favouriteData)
+            if (favouriteData.isNotEmpty())
+                localDataSource.setFavouriteData(favouriteData)
+        }
     }
 
     override suspend fun setFavourite(productId: String, favourite: Boolean) {
